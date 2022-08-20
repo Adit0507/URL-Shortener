456bd11ca267c0953b8af2ca29773f915eb19abb
Adit0507 <adi05072002@gmail.com>
2022-08-21 00:23:49 +0530

    Completed Showcase component

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -2,21 +2,28 @@ import React from "react";
 import showcase from "../images/illustration-working.svg";
 
 const Showcase = () => {
-  return <>
-    <section className="py-10 lg:py-20">
+  return (
+    <>
+      <section className="py-10 lg:py-20">
         <div className="max-width grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-10 md:place-items-center">
-            <article className="text-center md:text-left">
-                <h1 className="text-5xl lg:text-6xl mb-5 font-bold text-slate-800">
-                    More than just Shorter Links
-                </h1>
-                <p className="lg:text-lg text-slate-400 mb-10">
-                    Build Your Brand recognition a
-                </p>
-                <button className="bg-red-500 hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded">Get Started</button>
-            </article>
+          <article className="text-center md:text-left">
+            <h1 className="text-5xl lg:text-6xl mb-5 font-bold text-slate-800">
+              More than just shorter links
+            </h1>
+            <p className="lg:text-lg text-slate-400 mb-10">
+              Build your brand’s recognition and get detailed insights on how
+              your links are performing.
+            </p>
+            <button className="btn-ga rounded-">Get Started</button>
+          </article>
+
+          <article>
+            <img src={showcase} alt="Shortly" />
+          </article>
         </div>
-    </section>
-  </>;
+      </section>
+    </>
+  );
 };
 
 export default Showcase;
