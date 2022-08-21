bb41bb8fbb185d253bab91f75ddce20231dee7c0
Adit0507 <adi05072002@gmail.com>
2022-08-21 11:32:51 +0530

    Working on the advanced component

---
+++ src/components/Advanced.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import brand from "../images/icon-brand-recognition.svg"
+import records from "../images/icon-detailed-records.svg"
+import fully from "../images/icon-fully-customizable.svg"
+
+const Advanced = () => {
+  return (
+    <>
+        <section className='bg-gray-100 pt-32 pb-10 lg:pb-40'>
+            <div className='max-width'>
+                <h2 className='text-4xl font-bold text-slate-800 mb-3 text-center'>
+                    Advanced Statistics
+                </h2>
+                <p className='text-slate-400 text-center mb-10 lg:mb-20'>
+                    Track how 
+                </p>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Advanced
