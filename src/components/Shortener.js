58ac06ed2752babd7208a235eee460461efe8b48
Adit0507 <adi05072002@gmail.com>
2022-08-21 11:24:04 +0530

    Shortener component finished

--- src/components/Shortener.js
+++ src/components/Shortener.js
@@ -1,58 +1,102 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import bgMobile from "../images/bg-shorten-mobile.svg";
-import bgDesktop from "../images/bg-boost-desktop.svg"
+import bgDesktop from "../images/bg-boost-desktop.svg";
 
 const getLocalStorage = () => {
-    let links = localStorage.getItem("links");
+  let links = localStorage.getItem("links");
 
-    if(links){
-        return JSON.parse(localStorage.getItem("links"))
-    }else{
-        return []
-    }
-}
+  if (links) {
+    return JSON.parse(localStorage.getItem("links"));
+  } else {
+    return [];
+  }
+};
 
 const Shortener = () => {
-    const [text, setText] = useState("");
-    const [links, setLinks] = useState(getLocalStorage())
-    const [buttonText, setButtonText] = useState("Copy")
+  const [text, setText] = useState("");
+  const [links, setLinks] = useState(getLocalStorage());
+  const [buttonText, setButtonText] = useState("Copy");
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        if(!text){
-            alert("Empty Input 😐")
-        }else{
-            const shortenLink = async() => {
-                const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${text}`)
-                const data = await res.json();
-                console.log(data.result);
-                setLinks(data.result)
-                setText("");
-            }
-            shortenLink();
-        }
+    if (!text) {
+      alert("Empty Input 😐");
+    } else {
+      const shortenLink = async () => {
+        const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${text}`);
+        const data = await res.json();
+        console.log(data.result);
+        setLinks(data.result);
+        setText("");
+      };
+      shortenLink();
     }
+  };
 
-    const handleCopy = () =>{
-        navigator.clipboard.writeText(links.full_short_link)
-        setButtonText("Copied 😁")
-    }
+  const handleCopy = () => {
+    navigator.clipboard.writeText(links.full_short_link);
+    setButtonText("Copied 😁");
+  };
 
-    useEffect(() => {
-      localStorage.setItem("links", JSON.stringify(links));
-    }, [links])
-    
+  useEffect(() => {
+    localStorage.setItem("links", JSON.stringify(links));
+  }, [links]);
+
   return (
-        <>
-            <section className='max-width shortener relative'>
-                <picture>
-                    <source media="(min-width: 768px)" srcSet={bgDesktop} />
-                    <img src={bgMobile} alt="" />
-                </picture>
-            </section>
-        </>
-    )
-}
+    <>
+      <section className="max-width shortener relative">
+        <picture>
+          <source media="(min-width: 768px)" srcSet={bgDesktop} />
+          <img src={bgMobile} alt="" />
+        </picture>
 
-export default Shortener
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="flex flex-col md:flex-row">
+            <input
+              type="url"
+              placeholder="Shorten any link here 😉"
+              className="w-full py-2 px-5 rounded-lg mb-2 md:mb-0 md:w-2/3"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+
+            <button
+              type="submit"
+              className="bg-red-700 hover:bg-red-500 text-white font-bold py-2 px-4 border border-blue-700 rounded md:ml-5 md:w-40"
+              onClick={handleSubmit}
+            >
+              Shorten it !
+            </button>
+          </div>
+        </form>
+
+        <div className="flex flex-col items-center justify-center bg-white text-center md:flex-row md:justify-between p-3 mt-3 rounded-lg shadow">
+          <article>
+            <h6 className="mb-3 md:mb-0">{links.original_link}</h6>
+          </article>
+
+          <article>
+            <ul className="md:flex md:items-center">
+              <li className="md:mr-5">
+                <button className="text-purple-500">
+                  {links.full_short_link}
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={handleCopy}
+                  className="btn-cta rounded-lg text-sm focus:bg-slate-800"
+                >
+                  {buttonText}
+                </button>
+              </li>
+            </ul>
+          </article>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Shortener;
